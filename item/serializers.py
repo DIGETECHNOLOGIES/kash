afed2f9b0ec36bc033b1f36a1222f0d2dd33244e
cypherpet1 <smallbreedpuppies79@gmail.com>
2025-04-27 12:07:25 -0400

    Security Update

--- item/serializers.py
+++ item/serializers.py
@@ -1,8 +1,6 @@
 from .models import Item
-from shop.models import Category,Location,Image
+from shop.models import Category, Location, Image
 from rest_framework import serializers
-from django.core.exceptions import ValidationError
-# from shop.serializers import ImageSerializer
 
 class PriceValidator:
     def __call__(self, value):
@@ -10,51 +8,43 @@ class PriceValidator:
             raise serializers.ValidationError("Price must be a positive number")
         return value
 
-def validate_image_size(image):
-    max_size = 2 * 1024 * 1024  # 2MB
-    if image.size > max_size:
-        raise serializers.ValidationError("Each image size must not exceed 2MB")
-    return image
-
-
 class ImageSerializer(serializers.ModelSerializer):
     url = serializers.SerializerMethodField()
 
     class Meta:
-        model = Image  
+        model = Image
         fields = ['url']
 
     def get_url(self, obj):
-        request = self.context.get('request') 
-        if obj.image.url and request:
+        request = self.context.get('request')
+        if obj.image and request:
             return request.build_absolute_uri(obj.image.url)
         return None
 
-
 class ItemSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(max_length=255)
-    description = serializers.CharField(max_length=255)
     location = serializers.PrimaryKeyRelatedField(queryset=Location.objects.all())
     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
     current_price = serializers.DecimalField(max_digits=10, decimal_places=0, validators=[PriceValidator()])
-    images = ImageSerializer(many = True, required = False)
+    images = serializers.ListField(child=serializers.ImageField(), required=False)  # Allow image upload
 
     class Meta:
         model = Item
-        fields = ['id','name', 'current_price', 'description', 'location', 'category', 'images']
+        fields = ['id', 'name', 'current_price', 'description', 'location', 'category', 'images']
+        read_only_fields = ['id']  # Make 'id' read-only
 
     def create(self, validated_data):
-        images_data = validated_data.pop('images', [])  
-        item = Item.objects.create(**validated_data)  
+        images_data = validated_data.pop('images', [])
+        item = Item.objects.create(**validated_data)
 
         for image_data in images_data:
-            image = Image.objects.create(image=image_data)  
-            item.images.add(image)  
+            image = Image.objects.create(image=image_data)
+            item.images.add(image)  # Associate the image with the item
+
         item.save()
         return item
 
     def update(self, instance, validated_data):
-        images_data = validated_data.pop('images', [])  
+        images_data = validated_data.pop('images', [])
         instance.name = validated_data.get('name', instance.name)
         instance.current_price = validated_data.get('current_price', instance.current_price)
         instance.description = validated_data.get('description', instance.description)
@@ -62,10 +52,13 @@ class ItemSerializer(serializers.ModelSerializer):
         instance.category = validated_data.get('category', instance.category)
         instance.save()
 
+        # Handle image updates
         if images_data:
-            instance.images.clear()  
+            instance.images.clear()  # Remove all previous images
             for image_data in images_data:
                 image = Image.objects.create(image=image_data)
                 instance.images.add(image)
 
         return instance
+
+
--- item/views.py
+++ item/views.py
@@ -2,54 +2,48 @@ from django.shortcuts import get_object_or_404
 from rest_framework.generics import (
     ListAPIView, RetrieveAPIView, DestroyAPIView, CreateAPIView, UpdateAPIView
 )
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import ItemSerializer
 from shop.models import Item, Image
-
+from .permissions import IsOwnerPermission  
 
 class ItemList(ListAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
-    permission_classes = []
-
+    permission_classes = [AllowAny]  
 
 class ItemDetail(RetrieveAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     lookup_field = 'pk'
-    permission_classes = []
-
+    permission_classes = [AllowAny]  
 
 class CreateItem(CreateAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
-    permission_classes = []
-
+    permission_classes = [IsAuthenticated]  
     def perform_create(self, serializer):
         images_data = self.request.FILES.getlist('images') 
-        item = serializer.save()  
+        item = serializer.save(shop=self.request.user.shop)  
         for image_file in images_data:
             image = Image.objects.create(image=image_file)
-            item.images.add(image)  
-
+            item.images.add(image)
 
 class UpdateItem(UpdateAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     lookup_field = 'pk'
-    permission_classes = []
+    permission_classes = [IsAuthenticated, IsOwnerPermission]  
 
     def perform_update(self, serializer):
         images_data = self.request.FILES.getlist('images')  
         item = serializer.save()
-
         for image_file in images_data:
             image = Image.objects.create(image=image_file)  
             item.images.add(image)
-
 
 class DeleteItem(DestroyAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     lookup_field = 'pk'
-    permission_classes = []
+    permission_classes = [IsAuthenticated, IsOwnerPermission] 
